7623816b7cf0f78e7275bd103d6f268bcf72f545
JosephAnderson234 <joseph.cose@utec.edu.pe>
2026-05-23 13:53:56 -0500

    fix: Solved #54 implemented the hash service in to the search

--- src/modules/invitations/application/use-cases/obtain-info-use-case.ts
+++ src/modules/invitations/application/use-cases/obtain-info-use-case.ts
@@ -4,15 +4,20 @@ import {
     INVITATIONS_REPOSITORY,
 } from '@/modules/invitations/domain/port/invitations-repository.port';
 import { InvitationInfoDto } from '@/modules/invitations/application/dto/invitation-info.dto';
+import { HashServicePort } from '@/shared/domain/ports/hash-service.port';
 
 export class ObtainInfoUseCase {
     constructor(
         @Inject(INVITATIONS_REPOSITORY)
         private readonly invitationsRepository: InvitationsRepositoryPort,
+        @Inject(HashServicePort)
+        private readonly hashService: HashServicePort,
     ) {}
 
     async execute(token: string): Promise<InvitationInfoDto> {
-        const invitation = await this.invitationsRepository.findByToken(token);
+        const token_hash = this.hashService.hash(token);
+        const invitation =
+            await this.invitationsRepository.findByToken(token_hash);
         if (!invitation) {
             throw new Error('Token no encontrado');
         }
