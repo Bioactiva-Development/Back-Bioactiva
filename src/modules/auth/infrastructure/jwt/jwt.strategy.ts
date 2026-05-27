5ba5bec3b926bc4ce979c7a56fd84de1337cf384
JosephAnderson234 <joseph.cose@utec.edu.pe>
2026-05-27 14:49:21 -0500

    fix: simplify user validation check in JwtStrategy

--- src/modules/auth/infrastructure/jwt/jwt.strategy.ts
+++ src/modules/auth/infrastructure/jwt/jwt.strategy.ts
@@ -33,7 +33,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
             Number(payload.sub),
         );
 
-        if (!user || user.estado !== UserState.ACTIVO) {
+        if (user?.estado !== UserState.ACTIVO) {
             throw new UnauthorizedException('No autorizado');
         }
 
