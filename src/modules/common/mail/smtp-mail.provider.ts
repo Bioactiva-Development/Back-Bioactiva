12e481fb59bb1f5a064a038175134e8cbcb49f8e
JosephAnderson234 <joseph.cose@utec.edu.pe>
2026-05-27 15:12:28 -0500

    refactor: remove commented-out nodemailer code from SmtpMailProvider

--- src/modules/common/mail/smtp-mail.provider.ts
+++ src/modules/common/mail/smtp-mail.provider.ts
@@ -1,10 +1,8 @@
 import { MailProviderPort } from '@/modules/common/mail/mail-provider.port';
 import { UserRole } from '@/shared/domain/enums/rol';
 import { Injectable } from '@nestjs/common';
-//import nodemailer from 'nodemailer';
-//import { MailtrapTransport } from 'mailtrap'; is limited
 import { ResendMailProvider } from '@/modules/common/mail/resend.instance';
-//trying with resend
+
 @Injectable()
 export class SmtpMailProvider implements MailProviderPort {
     async sendInvitationEmail(input: {
@@ -13,20 +11,6 @@ export class SmtpMailProvider implements MailProviderPort {
         rol: UserRole;
         invitedBy: number;
     }): Promise<void> {
-        //Now we are going to use the resend provider, but we can switch to nodemailer if needed, just need to uncomment the code and add the mailtrap token to the env variables, also need to install the mailtrap package and nodemailer
-        /* const transporter = nodemailer.createTransport(
-            MailtrapTransport({
-                token: process.env.MAILTRAP_TOKEN || '',
-                //sandbox: process.env.NODE_ENV !== 'production',
-                //testInboxId: 4650783,
-            }),
-        ); */
-
-        //const sender = {
-        //    address: 'hello@demomailtrap.co',
-        //    name: 'Mailtrap Code',
-        //};
-
         const resendProvider = new ResendMailProvider();
 
         await resendProvider.sendInvitationEmail({
@@ -35,12 +19,6 @@ export class SmtpMailProvider implements MailProviderPort {
             rol: input.rol,
             invitedBy: input.invitedBy,
         });
-        /* await transporter.sendMail({
-            from: sender,
-            to: input.correo,
-            subject: 'Invitación a Back Bioactiva',
-            html: `<p>Has sido invitado.</p><p><a href="${invitationLink}">Aceptar invitación</a></p>`,
-        }); */
     }
 
     async sendResetPasswordEmail(input: {
